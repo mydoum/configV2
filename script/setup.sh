102e640a55136ad8b333c92c8489f4a38bcaac17
mydoum <a.alonso.giraldo@gmail.com>
2016-12-09 17:26:07 +0100

    Fixing the variable

--- script/setup.sh
+++ script/setup.sh
@@ -13,31 +13,25 @@
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
 DOTFILES_DIR=$HOME/dotfiles
-DOTFILE_SCRIPTS_DIR=$DOTFILES_DIR/scripts
+DOTFILE_SCRIPTS_DIR=$DOTFILES_DIR/script
 
 # ===========================================
 # Git
 # ===========================================
 if [ ! -d $DOTFILES_DIR ]; then
-  if hash git 2>/dev/null; then
-    echo "Git is already installed. Cloning repository..."
-    git clone git@github.com:mydoum/dotfiles.git $DOTFILES_DIR
-  else
-    echo "Git is not installed. Installing it..."
-    #Since macport is by default installed, we use it
-    port selfupdate
-    port install git
-    git clone git@github.com:mydoum/dotfiles.git $DOTFILES_DIR
-    # TODO: Uninstall macport when homebrew is installed
-  fi
-else
-  cd $DOTFILES_DIR
-  # We could have modifications in the repository, so we stash them
-  git stash
-  git pull origin master
-  git stash pop
+    if hash git 2>/dev/null; then
+        echo "Git is already installed. Cloning repository..."
+        git clone git@github.com:mydoum/dotfiles.git $DOTFILES_DIR
+    else
+        echo "Git is not installed. Installing it..."
+        #Since macport is by default installed, we use it
+        port selfupdate
+        port install git
+        git clone git@github.com:mydoum/dotfiles.git $DOTFILES_DIR
+        # TODO: Uninstall macport when homebrew is installed
+    fi
 fi
 
 # Source and launch
-source DOTFILE_SCRIPTS_DIR/setup/osx.sh
+source $DOTFILE_SCRIPTS_DIR/setup/osx.sh
 
