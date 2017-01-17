bc5183c36e6e46850b36ec81b8a21a6b3760c635
mydoum <a.alonso.giraldo@gmail.com>
2017-01-17 12:07:38 +0100

    [ADD] Adding meteor

--- script/npm.sh
+++ script/npm.sh
@@ -5,3 +5,14 @@ npm update
 
 notice "Installing npm dependencies"
 npm install nodemon -g
+npm install gulp -g
+
+notice "Installing meteor"
+if ! type meteor; then
+    curl https://install.meteor.com/ | sh
+    sudo chown -R $USER ~/.meteor
+    ln -s ~/.meteor/meteor /usr/local/bin
+    success "meteor successfully installed"
+else
+    debug "Meteor already installed"
+fi
