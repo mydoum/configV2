c4153aa01338472dd144bf680cb0defaa67f7b43
mydoum <aonn18@gmail.com>
2017-04-28 15:14:19 +0200

    adding xclip

--- linux/ubuntu/install.sh
+++ linux/ubuntu/install.sh
@@ -9,3 +9,12 @@ apt-get install firefox
 sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
 sudo apt-get update
 sudo apt-get install ubuntu-make
+
+apt-get install git
+
+# Install nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - 
+sudo apt-get install -y nodejs
+
+sudo apt-get install npm
+sudo apt-get install xclip
